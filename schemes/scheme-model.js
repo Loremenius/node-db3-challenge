4e68e58cf07fe27cee3ade86f5cca73ff18880ec
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-12-11 11:57:46 -0700

    added dbConfig in data and added basic functions into scheme-model.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,35 @@
+const db = require('../data/dbConfig');
+
+module.exports={
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+
+}
+
+function find(){
+    return db("schemes");
+}
+
+function findById(id){
+    return db("schemes").where("id", id).first();
+}
+
+function findSteps(id){
+    return db("steps").where("scheme_id", id);
+}
+
+function add(scheme){
+    return db.insert(scheme,"id").into("schemes");
+}
+
+function update(changes, id){
+    return db("schemes").where("id", id).update(changes);
+}
+
+function remove(id){
+    return db("scheme").where("id", id).del();
+}
