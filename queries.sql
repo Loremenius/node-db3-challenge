eab713338f24b106a82cbb20f9b30cc9d65f4c92
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-12-11 14:19:27 -0700

    completed stretch queries and fixed first query so that they join on category.id=product.categoryid

--- queries.sql
+++ queries.sql
@@ -1,10 +1,17 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-SELECT Product.ProductName, Category.CategoryName FROM Product JOIN Category ON Product.Id=Category.Id;
+SELECT Product.ProductName, Category.CategoryName FROM Product JOIN Category ON Category.Id=Product.categoryId;
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT [Order].Id, Shipper.CompanyName FROM [Order] JOIN Shipper ON [Order].ShipVia=Shipper.Id WHERE DATE([Order].OrderDate) < DATE('2012-08-09');
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 SELECT OrderDetail.Quantity, Product.ProductName FROM OrderDetail JOIN Product ON OrderDetail.ProductId=Product.Id WHERE OrderDetail.OrderId=10251 ORDER BY Product.ProductName;
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 SELECT [Order].Id, Customer.CompanyName, Employee.LastName FROM [Order] Join Customer ON [Order].CustomerId=Customer.Id JOIN Employee ON [Order].EmployeeId=Employee.Id;
+
+
+--Strech Using w3schools: Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 9 records.
+SELECT Categories.CategoryName, SUM(Products.CategoryId=Categories.CategoryId) AS Count FROM Categories Join Products ON Products.CategoryId=Categories.CategoryId GROUP BY Products.CategoryId;
+
+--Display OrderID and a column called ItemCount that shows the total number of products placed on the order. Shows 196 records.
+SELECT OrderID, SUM(Quantity) AS itemCount FROM OrderDetails GROUP BY OrderID;
